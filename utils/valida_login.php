3c46bb6a91854c3e47de36a629ce64ccdaa2ad52
pedrojaraujo <pedroa990@gmail.com>
2024-02-20 15:49:07 -0300

    Reformulando lógica para login

--- utils/valida_login.php
+++ utils/valida_login.php
@@ -1,14 +1,44 @@
 <?php
 
-/* $nome = $_POST["login"];
-$senha = $_POST["senha"];
+session_start();
 
-if (!$nome || !$senha) {
-    return;
-} else if ($nome === "admin" && $senha === "1234") {
-    echo "<p class=" . "login" . "> Passou </p>";
+// Usuários válidos
+$users_app = [
+    [
+        'email' => 'adm@teste.com.br',
+        'senha' => '123456'
+    ],
+
+    [
+        'email' => 'user@teste.com.br',
+        'senha' => 'abcd'
+    ],
+    [
+        'email' => 'admin',
+        'senha' => '1234'
+    ]
+];
+
+if (empty($_POST['user__email']) || empty($_POST['user__password'])) {
+    // Usuário não preencheu todos os campos
+    header('Location: ../pages/login.php?login=erro2');
 } else {
-    echo "<p class=" . "login" . "> Login ou Senha inválidos";
-} */
+    $user_authentication = false;
 
-?>
+    foreach ($users_app as $user) {
+        if ($user['email'] == $_POST['user__email'] && $user['senha'] == $_POST['user__password']) {
+            $user_authentication = true;
+            break;
+        }
+    }
+
+    if ($user_authentication) {
+        // Usuário autenticado com sucesso
+        $_SESSION['autenticado'] = 'SIM';
+        header('Location: ../pages/home.php');
+    } else {
+        // Autenticação falhou
+        $_SESSION['autenticado'] = 'NAO';
+        header('Location: ../pages/login.php?login=erro');
+    }
+}
