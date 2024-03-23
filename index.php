ddb4faada2f5a62facfb2e51178887242dbd7626
pedrojaraujo <pedroa990@gmail.com>
2024-03-22 22:43:43 -0300

    Inovação de visual

--- index.php
+++ index.php
@@ -4,26 +4,61 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Página inicial</title>
+    <title>Página Inicial</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
     <link rel="stylesheet" href="styles/globalStyles.css">
 </head>
 
 <body>
     <header class="header__container">
-        <div class="logo__container">
-            <img src="images/php-logo.svg" alt="Logo" width="150px">
+        <div class="nav__container">
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <div class="container-fluid">
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">Página Inicial</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Link</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Dropdown
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a class="dropdown-item" href="#">Ação</a></li>
+                                    <li><a class="dropdown-item" href="#">Outra ação</a></li>
+                                    <li>
+                                        <hr class="dropdown-divider">
+                                    </li>
+                                    <li><a class="dropdown-item" href="#">Algo mais aqui</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <form class="d-flex">
+                            <input class="form-control me-2" type="search" placeholder="Pesquisar..." aria-label="Search">
+                            <button class="btn btn-primary" type="submit">Pesquisar</button>
+                        </form>
+                    </div>
+                </div>
+            </nav>
         </div>
-        <div class="list__container">
-            <ul>
-                <li><a href="#">Login</a></li>
-                <li><a href="#">Cadastro</a></li>
-            </ul>
+        <div class="login__container">
+            <div class="logo__container">
+                <img src="images/logo.svg" alt="Logo" width="100px">
+            </div>
+            <div class="list__container">
+                <ul>
+                    <li><a href="#" class="btn btn-primary">Entrar</a></li>
+                    <li><a href="#" class="btn btn-primary">Cadastre-se</a></li>
+                </ul>
+            </div>
         </div>
     </header>
     <main>
         <section class="hero__section">
-            
+
         </section>
     </main>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous"></script>
