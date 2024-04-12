0bd07cee6341d12cecf0a0a63683bdcc5f786659
pedrojaraujo <pedroa990@gmail.com>
2024-04-12 17:43:17 -0300

    ajuste de função listar produto, e implementação de botões para editar e excluir produto.

--- pages/listar_produtos.php
+++ pages/listar_produtos.php
@@ -26,7 +26,9 @@ try {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Produtos</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="../styles/globalStyles.css">
 
 
@@ -43,27 +45,31 @@ try {
                     <th>Nome</th>
                     <th>Descrição</th>
                     <th>Preço</th>
-                    <th>Imagem</th>
+                    <!--  <th>Imagem</th> -->
                     <th>Ações</th>
                 </tr>
             </thead>
 
             <?php foreach ($produtos as $produto) : ?>
-                <tr>
-                    <td><?php echo $produto['id_produto']; ?></td>
-                    <td><?php echo $produto['nome']; ?></td>
-                    <td><?php echo $produto['descricao']; ?></td>
-                    <td><?php echo $produto['preco']; ?></td>
-                    <td> <img src="<?php echo "../uploads/{$produto['imagem']}"; ?>" width="50" alt="imagem do produto" /> </td>
-                    <td>
-                        <a href="../utils/editar_produto.php?id=<?php echo $produto['id_produto'] ?>" class="btn btn-primary">Editar</a>
-                        <a href="../utils/excluir_produto.php?id=<?php echo $produto['id_produto'] ?>" class="btn btn-danger">Deletar</a>
-                    </td>
-                </tr>
+            <tr>
+                <td><?php echo $produto['PRODUTO_ID']; ?></td>
+                <td><?php echo $produto['PRODUTO_NOME']; ?></td>
+                <td><?php echo $produto['PRODUTO_DESC']; ?></td>
+                <td><?php echo $produto['PRODUTO_PRECO']; ?></td>
+                <!--  <td> <img src="<?php echo "../uploads/{$produto['imagem']}"; ?>" width="50" alt="imagem do produto" /> -->
+                </td>
+                <td>
+                    <a href="../utils/editar_produto.php?id=<?php echo $produto['PRODUTO_ID'] ?>"
+                        class="btn btn-primary">Editar</a>
+                    <a href="../utils/excluir_produto.php?id=<?php echo $produto['PRODUTO_ID'] ?>"
+                        class="btn btn-danger">Deletar</a>
+                </td>
+            </tr>
             <?php endforeach ?>
         </table>
 
-        <a href="painel_adm.php" class="btn btn-primary"><i class="fa-solid fa-arrow-rotate-left"></i> Voltar ao Painel do Administrador</a>
+        <a href="painel_adm.php" class="btn btn-primary"><i class="fa-solid fa-arrow-rotate-left"></i> Voltar ao Painel
+            do Administrador</a>
     </section>
 
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
---
+++ utils/editar_produto.php
@@ -0,0 +1,6 @@
+<?php
+$id = $_GET['id'];
+
+if (isset($id)) {
+    echo '<p class="text-success">Está dando certo a identificação!</p>';
+}
