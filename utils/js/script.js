591361052d591bbb993683ee589367b4e0929729
Pedro Joaquim Araujo <pedroa990@gmail.com>
2024-04-17 12:23:07 -0300

    Finalizando botão de dark mode

--- utils/js/script.js
+++ utils/js/script.js
@@ -1,11 +1,15 @@
-const body = document.querySelector('body');
-sidebar = body.querySelector('.sidebar');
-toggle = body.querySelector('.toggle');
-mode = body.querySelector('.mode');
-modeSwitch = body.querySelector('.toggle-switch');
-modeText = body.querySelector('.mode__text');
+const body = document.querySelector("body");
+const sidebar = body.querySelector(".sidebar");
+const toggle = body.querySelector(".toggle");
+const mode = body.querySelector(".mode");
+const modeSwitch = body.querySelector(".toggle-switch");
+const modeText = body.querySelector(".mode__text");
 
-
-modeSwitch.addEventListener('click', () => {
-    body.classList.toggle('dark');
+modeSwitch.addEventListener("click", () => {
+  body.classList.toggle("dark");
+  if (modeText.innerHTML === "Dark Mode") {
+    modeText.innerHTML = "Light Mode";
+  } else {
+    modeText.innerHTML = "Dark Mode";
+  }
 });
