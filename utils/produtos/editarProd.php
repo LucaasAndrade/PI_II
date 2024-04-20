d9e64cb1ac52835b87fb77ebe3f8acb88f1a894f
pedrojaraujo <pedroa990@gmail.com>
2024-04-20 12:54:56 -0300

    ""

--- utils/produtos/editarProd.php
+++ utils/produtos/editarProd.php
@@ -13,9 +13,9 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $ativo = isset($_POST['ativo']) ? '1' : '';
     $categoria = $_POST['categoria_id'];
 
-    // Verifica se as imagens foram enviadas
+
     if (isset($_POST['imagem_url']) && isset($_POST['imagem_ordem']) && isset($_POST['imagem_id'])) {
-        // Recupera os arrays de imagens
+
         $imagem_urls = $_POST['imagem_url'];
         $imagem_ordens = $_POST['imagem_ordem'];
         $imagem_ids = $_POST['imagem_id'];
@@ -36,7 +36,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
                 $stmt_update->execute();
             }
 
-            // Atualiza as outras informações do produto
+
             $stmt_update_produto = $pdo->prepare("
                 UPDATE PRODUTO 
                 SET PRODUTO_NOME = :nome, PRODUTO_DESC = :descricao, 
@@ -53,7 +53,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             $stmt_update_produto->bindParam(':ativo', $ativo, PDO::PARAM_BOOL);
             $stmt_update_produto->execute();
 
-            // Atualiza a quantidade de estoque
+
             $stmt_update_estoque = $pdo->prepare("
                 UPDATE PRODUTO_ESTOQUE 
                 SET PRODUTO_QTD = :qtd 
@@ -68,7 +68,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
             header('Location: ../../pages/produtos/listar_produtos.php');
             exit();
         } catch (PDOException $e) {
-            // Se ocorrer algum erro, desfaz as alterações e exibe uma mensagem de erro
+
             $pdo->rollBack();
             echo "Erro ao alterar informações: " . $e->getMessage();
         }
