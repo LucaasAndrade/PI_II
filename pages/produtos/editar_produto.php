5d253f7b6b559883542d7e065fc15dbfeb7a1f5a
pedrojaraujo <pedroa990@gmail.com>
2024-04-20 12:45:04 -0300

    Editar produto concluído

--- pages/produtos/editar_produto.php
+++ pages/produtos/editar_produto.php
@@ -60,8 +60,11 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Lista produtos</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css"
+        integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA=="
+        crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="../../styles/globalstyles.css">
 
 </head>
@@ -77,47 +80,50 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 
             <div class="mb-3">
                 <label for="nome" class="form-label">Nome</label>
-                <input type="text" name="nome" id="nome" class="form-control" value="<?php echo $produto['PRODUTO_NOME']; ?>" required>
+                <input type="text" name="nome" id="nome" class="form-control"
+                    value="<?php echo $produto['PRODUTO_NOME']; ?>" required>
             </div>
             <div class="mb-3">
                 <label for="descricao" class="form-label">Descrição</label>
-                <textarea name="descricao" cols="30" rows="10" class="form-control" required><?php echo $produto['PRODUTO_DESC']; ?></textarea>
+                <textarea name="descricao" cols="30" rows="10" class="form-control"
+                    required><?php echo $produto['PRODUTO_DESC']; ?></textarea>
             </div>
             <div class="mb-3">
                 <label for="preco" class="form-label">Preço</label>
-                <input type="number" name="preco" class="form-control" value="<?php echo $produto['PRODUTO_PRECO']; ?>" required>
+                <input type="number" name="preco" class="form-control" value="<?php echo $produto['PRODUTO_PRECO']; ?>"
+                    required>
             </div>
             <div class="mb-3">
                 <label for="desconto" class="form-label">Desconto</label>
 
-                <input type="number" name="desconto" class="form-control" value="<?php echo $produto['PRODUTO_DESCONTO']; ?>" required>
+                <input type="number" name="desconto" class="form-control"
+                    value="<?php echo $produto['PRODUTO_DESCONTO']; ?>" required>
             </div>
             <div class="mb-3">
                 <label for="qtd_estoque" class="form-label">Quantidade estoque:</label>
-                <input type="number" name="qtd_estoque" class="form-control" value="<?php echo $produto['PRODUTO_QTD']; ?>" required>
+                <input type="number" name="qtd_estoque" class="form-control"
+                    value="<?php echo $produto['PRODUTO_QTD']; ?>" required>
             </div>
             <div class="mb-3">
-                <label for="imagem_url" class="form-label">Imagem URL:</label>
 
                 <?php
                 foreach ($imagens_produto as $imagem) {
-                    echo "<input type='text' name='imagem_url' class='form-control' value='{$imagem['IMAGEM_URL']}' required>";
-                }
+                    echo "<div class='mb-3'>";
+                    echo "<label for='imagem_url' class='form-label'>Imagem URL:</label>";
+                    echo "<input type='text' name='imagem_url[]' class='form-control' value='{$imagem['IMAGEM_URL']}' required>";
+                    echo "</div>";
 
-                foreach ($imagens_produto as $imagem) {
-                    echo "<input type='hidden' name='imagem_id' class='form-control' value='{$imagem['IMAGEM_ID']}' required>";
-                }
-                ?>
+                    echo "<div class='mb-3'>";
+                    echo "<label for='imagem_ordem' class='form-label'>Imagem Ordem:</label>";
+                    echo "<input type='number' name='imagem_ordem[]' class='form-control' value='{$imagem['IMAGEM_ORDEM']}' required>";
+                    echo "</div>";
 
-            </div>
-            <div class="mb-3">
-                <label for="imagem_ordem" class="form-label">Imagem Ordem</label>
-                <?php
-                foreach ($imagens_produto as $imagem) {
-                    echo "<input type='text' name='imagem_ordem' class='form-control' value='{$imagem['IMAGEM_ORDEM']}' required>";
+                    // Inclua o campo imagem_id como um input hidden
+                    echo "<input type='hidden' name='imagem_id[]' value='{$imagem['IMAGEM_ID']}'>";
                 }
                 ?>
             </div>
+
             <div class="mb-3">
                 <div class="mb-3">
                     <label for="categoria_id">Categoria: </label>
@@ -131,15 +137,18 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
                 </div>
                 <div class="mb-3">
                     <label for="ativo" class="form-label">Ativo</label>
-                    <input type="checkbox" name="ativo" value="1" <?php echo $produto['PRODUTO_ATIVO'] == 1 ? 'checked' : '' ?>>
+                    <input type="checkbox" name="ativo" value="1"
+                        <?php echo $produto['PRODUTO_ATIVO'] == 1 ? 'checked' : '' ?>>
                 </div>
                 <input type="submit" class="btn btn-primary" value="Enviar">
         </form>
 
     </section>
-    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js" integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"
+        integrity="sha384-IQsoLXl5PILFhosVNubq5LC7Qb9DXgDA9i+tQ8Zj3iwWAwPtgFTxbJ8NT4GN1R8p" crossorigin="anonymous">
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js"
+        integrity="sha384-cVKIPhGWiC2Al4u+LWgxfKTRIcfu0JTxR+EQDz/bgldoEyl4H0zUF0QKbrJ0EcQF" crossorigin="anonymous">
     </script>
 </body>
 
--- utils/produtos/editarProd.php
+++ utils/produtos/editarProd.php
@@ -2,6 +2,7 @@
 
 require_once '../conexao.php';
 
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $id = $_POST['id'];
     $nome = $_POST['nome'];
@@ -9,44 +10,69 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $preco = $_POST['preco'];
     $desconto = $_POST['desconto'];
     $qtd = $_POST['qtd_estoque'];
-    $imagem_url = $_POST['imagem_url'];
-    $imagem_ordem = $_POST['imagem_ordem'];
-    $imagem_id = $_POST['imagem_id'];
     $ativo = isset($_POST['ativo']) ? '1' : '';
     $categoria = $_POST['categoria_id'];
 
-    try {
+    // Verifica se as imagens foram enviadas
+    if (isset($_POST['imagem_url']) && isset($_POST['imagem_ordem']) && isset($_POST['imagem_id'])) {
+        // Recupera os arrays de imagens
+        $imagem_urls = $_POST['imagem_url'];
+        $imagem_ordens = $_POST['imagem_ordem'];
+        $imagem_ids = $_POST['imagem_id'];
 
-      
+        try {
+            $pdo->beginTransaction();
 
+            $stmt_update = $pdo->prepare("
+            UPDATE PRODUTO_IMAGEM 
+            SET IMAGEM_URL = :imagem_url, IMAGEM_ORDEM = :imagem_ordem
+            WHERE PRODUTO_ID = :id AND IMAGEM_ID = :imagem_id
+        ");
+            foreach ($imagem_urls as $key => $imagem_url) {
+                $stmt_update->bindParam(':id', $id, PDO::PARAM_INT);
+                $stmt_update->bindParam(':imagem_url', $imagem_url, PDO::PARAM_STR);
+                $stmt_update->bindParam(':imagem_ordem', $imagem_ordens[$key], PDO::PARAM_INT);
+                $stmt_update->bindParam(':imagem_id', $imagem_ids[$key], PDO::PARAM_INT);
+                $stmt_update->execute();
+            }
 
-        $stmt = $pdo->prepare("UPDATE PRODUTO_IMAGEM SET IMAGEM_ORDEM  = :ordem, IMAGEM_URL = :imagem_url, IMAGEM_ID = :imagem_id WHERE PRODUTO_ID  = :id");
-        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-        $stmt->bindParam(':imagem_url', $imagem_url, PDO::PARAM_STR);
-        $stmt->bindParam(':ordem', $imagem_ordem, PDO::PARAM_INT);
-        $stmt->bindParam(':imagem_id', $imagem_id, PDO::PARAM_INT);
-        $stmt->execute();
+            // Atualiza as outras informações do produto
+            $stmt_update_produto = $pdo->prepare("
+                UPDATE PRODUTO 
+                SET PRODUTO_NOME = :nome, PRODUTO_DESC = :descricao, 
+                    PRODUTO_PRECO = :preco, PRODUTO_DESCONTO = :desconto, 
+                    CATEGORIA_ID = :categoria, PRODUTO_ATIVO = :ativo 
+                WHERE PRODUTO_ID = :id
+            ");
+            $stmt_update_produto->bindParam(':id', $id, PDO::PARAM_INT);
+            $stmt_update_produto->bindParam(':nome', $nome, PDO::PARAM_STR);
+            $stmt_update_produto->bindParam(':descricao', $desc, PDO::PARAM_STR);
+            $stmt_update_produto->bindParam(':preco', $preco, PDO::PARAM_STR);
+            $stmt_update_produto->bindParam(':desconto', $desconto, PDO::PARAM_STR);
+            $stmt_update_produto->bindParam(':categoria', $categoria, PDO::PARAM_INT);
+            $stmt_update_produto->bindParam(':ativo', $ativo, PDO::PARAM_BOOL);
+            $stmt_update_produto->execute();
 
-        $stmt = $pdo->prepare("UPDATE PRODUTO_ESTOQUE SET PRODUTO_QTD = :qtd WHERE PRODUTO_ID  = :id");
-        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-        $stmt->bindParam(':qtd', $qtd, PDO::PARAM_STR);
-        $stmt->execute();
+            // Atualiza a quantidade de estoque
+            $stmt_update_estoque = $pdo->prepare("
+                UPDATE PRODUTO_ESTOQUE 
+                SET PRODUTO_QTD = :qtd 
+                WHERE PRODUTO_ID = :id
+            ");
+            $stmt_update_estoque->bindParam(':id', $id, PDO::PARAM_INT);
+            $stmt_update_estoque->bindParam(':qtd', $qtd, PDO::PARAM_INT);
+            $stmt_update_estoque->execute();
 
+            $pdo->commit();
 
-
-        $stmt = $pdo->prepare("UPDATE PRODUTO SET PRODUTO_NOME = :nome, PRODUTO_DESC = :descricao, PRODUTO_PRECO = :preco, PRODUTO_DESCONTO = :desconto, CATEGORIA_ID = :categoria,PRODUTO_ATIVO = :ativo WHERE PRODUTO_ID  = :id");
-        $stmt->bindParam(':id', $id, PDO::PARAM_INT);
-        $stmt->bindParam(':nome', $nome, PDO::PARAM_STR);
-        $stmt->bindParam(':descricao', $desc, PDO::PARAM_STR);
-        $stmt->bindParam(':preco', $preco, PDO::PARAM_STR);
-        $stmt->bindParam(':desconto', $desconto, PDO::PARAM_STR);
-        $stmt->bindParam(':categoria', $categoria, PDO::PARAM_INT);
-        $stmt->bindParam(':ativo', $ativo, PDO::PARAM_BOOL);
-        $stmt->execute();
-
-        header('Location: ../../pages/produtos/listar_produtos.php');
-        exit();
-    } catch (PDOException $e) {
-        echo "Erro ao alterar informações: " . $e->getMessage();
+            header('Location: ../../pages/produtos/listar_produtos.php');
+            exit();
+        } catch (PDOException $e) {
+            // Se ocorrer algum erro, desfaz as alterações e exibe uma mensagem de erro
+            $pdo->rollBack();
+            echo "Erro ao alterar informações: " . $e->getMessage();
+        }
+    } else {
+        echo "Erro: Não foram enviadas imagens para atualização.";
     }
 }
