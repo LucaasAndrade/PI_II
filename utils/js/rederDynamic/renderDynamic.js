9b881bd42a2713ebd467b591d9264f37864ae7e9
pedrojaraujo <pedroa990@gmail.com>
2024-04-21 14:07:58 -0300

    Adaptação do script para dynamic-section

--- utils/js/rederDynamic/renderDynamic.js
+++ utils/js/rederDynamic/renderDynamic.js
@@ -8,7 +8,19 @@ export function renderDynamic() {
       fetch(link.href)
         .then((response) => response.text())
         .then((data) => {
-          document.querySelector("#dynamic-content").innerHTML = data;
+          const tempDiv = document.createElement("div");
+          tempDiv.innerHTML = data;
+          const formContainer = tempDiv.querySelector(".dynamic-section");
+          if (formContainer) {
+            document.querySelectorAll("#dynamic-content").forEach((element) => {
+              element.innerHTML = formContainer.innerHTML;
+            });
+          } else {
+            console.log("Não foi possível carregar .form__container");
+          }
+        })
+        .catch((error) => {
+          console.log("Error fetching content:", error);
         });
     });
   });
