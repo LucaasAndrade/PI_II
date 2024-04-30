f00e7dc3297f00ee6e1c4b01c61135c462d54d7d
pedrojaraujo <pedroa990@gmail.com>
2024-04-29 22:30:02 -0300

    Adaptação do script para iniciar Glide.js

--- utils/js/script.js
+++ utils/js/script.js
@@ -2,7 +2,7 @@ import { adicionarImagem } from "./adicionarImagem/adicionarImagem.js";
 import { editAdm } from "./editAdm/editAdm.js";
 import { alterMode } from "./mode/mode.js";
 import { sideBarOpen } from "./sidebar/sideBarOpen.js";
-import  {renderDynamic } from "./rederDynamic/renderDynamic.js"
+import { renderDynamic } from "./rederDynamic/renderDynamic.js";
 import { editProd } from "./editProd/editProd.js";
 
 sideBarOpen();
