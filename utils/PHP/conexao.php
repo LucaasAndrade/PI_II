33d0f08a7b26f96745c18c5855723e530a495d23
pedrojaraujo <pedroa990@gmail.com>
2024-05-24 17:23:44 -0300

    Add conexão bd externo Echo

--- utils/PHP/conexao.php
+++ utils/PHP/conexao.php
@@ -1,7 +1,8 @@
 <?php
 session_start();
 
-$host = 'localhost';
+
+/* $host = 'localhost';
 $db = 'echo';
 $user = 'root';
 $pass = '';
@@ -14,4 +15,22 @@ try {
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (PDOException $e) {
     echo "Error connecting to the database: " . $e->getMessage();
+}
+ */
+
+
+$host = '144.22.157.228';
+$port = '3306';
+$db = 'Echo';
+$user = 'echo';
+$pass = 'echo';
+$charset = 'utf8mb4';
+
+$dsn = "mysql:host=$host;port=$port;dbname=$db;charset=$charset";
+
+try {
+ $pdo = new PDO($dsn, $user, $pass);
+ $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+ echo "Error connecting to the database: " . $e->getMessage();
 }
